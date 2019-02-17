579a9dd7a8e9983363f77546d57a7c788a333380
Nikita <nikitauljo9@gmail.com>
2019-02-17 16:16:17 +0300

    Update WordsManipulation.cs

--- WordsManipulation.cs
+++ WordsManipulation.cs
@@ -10,35 +10,55 @@ namespace PadawansTask8
             if (text == null) throw new ArgumentNullException();
             if (text.Length == 0) throw new ArgumentException();
 
-            string[] z = text.Split(new char[] { ' '}, StringSplitOptions.RemoveEmptyEntries);
-            string[] a = text.Split(new char[] { ' ','.',',','-','!','?',';',':' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string[] a = text.Split(new char[] { ' ', '.', ',', '-', '!', '?', ';', ':' }, StringSplitOptions.RemoveEmptyEntries);
+            int index = 0;
             int s = a.Length - 1;
-            text = "";
 
             for (int q = 0; q < a.Length; q++)
             {
                 for (int i = 0; i < a.Length; i++)
                 {
+                    string v = a[i];
                     if (i == q) i++;
                     if (q == s) break;
-                    if (a[i] == a[q])
-                    {
-                        a[i] = "";
-                    }
 
+
+                    if (a[i] == a[q]) { a[i] = ""; }
                 }
 
-                if (q == s && a[q]=="") break;
-                if (q == s && a[q] != "") { text += z[q]; break; }
-
-                if (a[q] == "") text += " ";
-                else
-                {
-                    text += z[q] + " ";
-                }
             }
 
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                int b = 0;
+                if (a[i] == "") continue;
+
+                string v = a[i];
+                while (b < v.Length)
+                {
+                    if (!char.IsLetter(v[b]))
+                    {
+                        break;
+                    }
+                    b++;
+                }
+                if (b != v.Length) continue;
+
+
+                index = text.IndexOf(a[i]);
+                if (index != -1)
+                {
+                    if (a[i].EndsWith(a[i]))
+                    {
+
+                        text = text.Replace(a[i], "");
+                        text = text.Insert(index, a[i]);
+                    }
+                }
+            }
         }
+
     }
 }
-
