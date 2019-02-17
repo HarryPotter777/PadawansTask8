1906882b0aa7b6ae110d1cf92ad9732d957a22b1
Nikita <nikitauljo9@gmail.com>
2019-02-17 18:41:48 +0300

    Update WordsManipulation.cs

--- WordsManipulation.cs
+++ WordsManipulation.cs
@@ -10,53 +10,61 @@ namespace PadawansTask8
             if (text == null) throw new ArgumentNullException();
             if (text.Length == 0) throw new ArgumentException();
 
-
-            string[] a = text.Split(new char[] { ' ', '.', ',', '-', '!', '?', ';', ':' }, StringSplitOptions.RemoveEmptyEntries);
-            int index = 0;
+            string[] a = text.Split(new char[] { ' ' });
             int s = a.Length - 1;
 
             for (int q = 0; q < a.Length; q++)
             {
+                string e = a[q];
+                int m = (e.Length - 1);
+                if (e.Length == 0) continue;
+                while ((e[m] == ' ' || e[m] == ',' || e[m] == '!' || e[m] == '?' || e[m] == '-' || e[m] == '.' || e[m] == ';' || e[m] == ':'))
+                {
+                    e = e.Substring(0, m);
+                    m--;
+                }
+
                 for (int i = 0; i < a.Length; i++)
                 {
-                    string v = a[i];
+
                     if (i == q) i++;
                     if (q == s) break;
 
+                    string v = a[i];
 
-                    if (a[i] == a[q]) { a[i] = ""; }
+                    if (v.Length == 0) continue;
+
+                    int b = 0;
+                    int c = (v.Length - 1);
+
+                    while (v[c] == ' ' || v[c] == ',' || v[c] == '!' || v[c] == '?' || v[c] == '-' || v[c] == '.' || v[c] == ';' || v[c] == ':')
+                    {
+                        v = v.Substring(0, c);
+                        c--;
+                    }
+                    while (b < v.Length)
+                    {
+                        if (!char.IsLetter(v[b]))
+                        {
+                            break;
+                        }
+                        b++;
+                    }
+                    if (b != v.Length) continue;
+
+
+                    if (v == e) { a[i] = ""; }
                 }
+
 
             }
+            text = "";
 
 
-            for (int i = 0; i < a.Length; i++)
+            for (int r = 0; r < a.Length; r++)
             {
-                int b = 0;
-                if (a[i] == "") continue;
-
-                string v = a[i];
-                while (b < v.Length)
-                {
-                    if (!char.IsLetter(v[b]))
-                    {
-                        break;
-                    }
-                    b++;
-                }
-                if (b != v.Length) continue;
-
-
-                index = text.IndexOf(a[i]);
-                if (index != -1)
-                {
-                    if (a[i].EndsWith(a[i]))
-                    {
-
-                        text = text.Replace(a[i], "");
-                        text = text.Insert(index, a[i]);
-                    }
-                }
+                if (r == a.Length - 1) text += a[r];
+                else text += a[r] + " ";
             }
         }
 
